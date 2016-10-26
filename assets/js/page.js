e8d145131135068a5e63c889ccc7acd807ba04ca
Kristof Dewilde <me@krivi.be>
2016-10-26 15:26:49 +0200

    asdf

--- assets/js/page.js
+++ assets/js/page.js
@@ -6,11 +6,13 @@ $( document ).ready( function(){
         }, {
             duration : 1000,
             complete : function(){
-                $.force_appear();
                 $( '.pace' ).remove();
+                $.force_appear();
             }
         } );
     } );
+
+    $('body > header p, .h').appear();
 
     $( 'a[href*="#"]:not([href="#"])' ).click( function(){
         if( location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' ) && location.hostname == this.hostname ){
@@ -31,6 +33,66 @@ $( document ).ready( function(){
         }
     } );
 
+    $( 'input, textarea' ).on( 'blur', function(){
+        $( this ).removeClass( 'error' );
+    } );
+
+    $( 'body > header p' ).on( 'appear', function(){
+        $( this ).css( 'animation','fadeIn 1250ms forwards' );
+    } );
+
+    $( '.h' ).on( 'appear', function(){
+        $( this ).removeClass( 'h' );
+    } );
+
+    $( '#contact' ).submit( function( e ){
+        e.preventDefault();
+
+        var errors = false;
+
+        $( '*' ).removeClass( 'error' );
+        $( 'span', this ).text( ' ' );
+
+        if( $( '[name=email]', this ).val().indexOf( '@' ) < 0 ){
+            $( '[name=email]', this ).addClass( 'error' );
+            $( '#contact span' ).addClass( 'error' );
+            $( '#contact span' ).text( 'Gelieve een geldig e-mailadres op te geven.' );
+            errors = true;
+        }
+
+        $( 'input, textarea', this ).each( function(){
+            if( !$( this ).val().trim() ){
+                $( this ).addClass( 'error' );
+                $( '#contact span' ).addClass( 'error' );
+                $( '#contact span' ).text( 'Alle velden zijn verplicht.' );
+                errors = true;
+            }
+        } );
+
+        if( errors ){
+            for( var i = 0; i < 5; i++ ){
+                $( 'button', this ).animate( {
+                    marginRight : ((i % 2 == 0 ? 15 : 15 * -1))
+                }, 75 );
+            }
+            $( 'button', this ).animate( { marginRight : 0 }, 75 );
+        }else{
+            $.post( $( '#contact' ).attr( 'action' ), $( '#contact' ).serialize(), function(){
+                $( '#contact *' ).removeClass( 'error' );
+                $( '#contact span' ).text( ' ' );
+            } )
+                .done( function(){
+                    $( '#contact *:not(span)' ).slideUp();
+                    $( '#contact span' ).text( 'Je bericht werd verzonden! We antwoorden zo spoedig mogelijk!' );
+                } )
+                .fail( function(){
+                    $( '#contact span' ).addClass( 'error' );
+                    $( '#contact span' ).text( 'Er ging iets goed mis! Je bericht kon niet verzonden worden.' );
+                } );
+        }
+        return false;
+    } );
+
     loadMap();
 } );
 
@@ -43,12 +105,12 @@ function loadMap(){
     var map = new mapboxgl.Map( {
         container : 'map',
         style : 'mapbox://styles/qrivi/ciuk8d6jb00ar2jmlqkwgbsrg',
-        center : [3.209271,51.202320],
-        zoom : 15
+        center : [3.209271, 51.202320],
+        zoom : 13
     } );
 
     new mapboxgl.Marker( el )
-        .setLngLat( [3.209271,51.202320] )
+        .setLngLat( [3.209271, 51.202320] )
         .addTo( map );
 
     map.addControl( new mapboxgl.NavigationControl() );
